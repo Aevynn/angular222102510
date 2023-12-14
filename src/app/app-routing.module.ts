7383b8a078cbd39aab955847dc411a42a1e7f9b9
Aevynn <rivaldobriliandy16@gmail.com>
2023-12-14 11:35:15 +0700

    cuaca

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { CuacaComponent } from './cuaca/cuaca.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Dashboard2Component } from './dashboard2/dashboard2.component';
@@ -17,6 +18,7 @@ const routes: Routes = [
     component: DashboardComponent,
     canActivate: [otentikasiGuard],
   },
+  { path: 'cuaca', component: CuacaComponent, canActivate: [otentikasiGuard] },
   {
     path: 'dashboard2',
     component: Dashboard2Component,
